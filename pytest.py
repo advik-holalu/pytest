6abc90b4fb47c98b39dbdf795213737faa67a029
advik-holalu <110801514+advik-holalu@users.noreply.github.com>
2023-10-06 16:36:32 +0530

    Add files via upload

---
+++ pytest.py
@@ -0,0 +1,11 @@
+# This is a simple program that adds two numbers and prints the result.
+
+# Define two variables
+a = 3
+b = 3
+
+# Calculate their sum
+result = a + b
+
+# Print the result
+print(result)
